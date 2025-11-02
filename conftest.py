ac913a455dc1f79af8d273c5323fd35bae77dc6e
Vladimir Kozhevnikov <kozhevnikovulstu@yandex.ru>
2025-11-02 20:08:47 +0400

    Добавил опции браузера для запуска CI/CD

--- conftest.py
+++ conftest.py
@@ -1,11 +1,19 @@
 import pytest
 from data.urls import Urls
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 @pytest.fixture
 def driver():
-    driver = webdriver.Chrome()
-    driver.maximize_window()
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--window-size=1920,1080')
+
+    driver = webdriver.Chrome(options=options)
+    driver.implicitly_wait(10)
     driver.get(Urls.XYZ_BANK_URL)
     yield driver
     driver.quit()
