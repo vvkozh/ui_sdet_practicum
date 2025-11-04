f72fd2a16952267dcf897510bd0777fdd8ed1c21
Vladimir Kozhevnikov <kozhevnikovulstu@yandex.ru>
2025-11-04 14:16:05 +0400

    Добавил проверку сортировку обоих случаев

--- tests/test_sort_customers.py
+++ tests/test_sort_customers.py
@@ -1,5 +1,4 @@
 import allure
-
 from pages.xyz_bank_page import XYZBankPage
 
 @allure.epic('Операции с пользователями')
@@ -18,8 +17,16 @@ def test_sort_name(driver):
             sort_name.click_sort_name()
             sort_asc = sort_name.get_name_customers()
             assert sort_asc == sorted(customers_name), 'Имена не отсортированы по возрастанию A-Z'
+        with allure.step('Проверка сортировка по убыванию Z-A'):
+            sort_name.click_sort_name()
+            sort_desc = sort_name.get_name_customers()
+            assert sort_desc == sorted(customers_name, reverse=True), 'Имена не отсортированы по убыванию Z-A'
     else:
         with allure.step('Проверка сортировка по убыванию Z-A'):
             sort_name.click_sort_name()
             sort_desc = sort_name.get_name_customers()
             assert sort_desc == sorted(customers_name, reverse=True), 'Имена не отсортированы по убыванию Z-A'
+        with allure.step('Проверка сортировка по возрастанию A-Z'):
+            sort_name.click_sort_name()
+            sort_asc = sort_name.get_name_customers()
+            assert sort_asc == sorted(customers_name), 'Имена не отсортированы по возрастанию A-Z'
